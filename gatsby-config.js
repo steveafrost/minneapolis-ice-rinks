b9a3cc81cce95fc33c980315094bcc95c7107467
Steve Frost <sfrost@nerdery.com>
2018-12-14 22:05:17 -0600

    update config w/ current metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Minneapolis Ice Rinks',
+    description:
+      'Find a list of the local Minneapolis ice rinks w/ their current open or closed status',
+    author: '@steveeff',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -18,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'minneapolis-ice-rinks',
+        short_name: 'minneapolis-ice-rinks',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
